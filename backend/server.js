2ee9f9982f5f695f30c228b897ab0c6330070ed1
Abdullah-FSWD <ansarimdabdullah794@gmail.com>
2023-12-13 11:46:59 +0530

    some code improvement

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 import express from "express";
 import data from "./data.js";
-
 const app = express();
 
 app.get("/api/products", (req, res) => {
@@ -9,6 +8,15 @@ app.get("/api/products", (req, res) => {
 
 app.get("/api/products/slug/:slug", (req, res) => {
   const product = data.products.find((x) => x.slug === req.params.slug);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product Not Found" });
+  }
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
   if (product) {
     res.send(product);
   } else {
