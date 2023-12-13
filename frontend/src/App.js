edbb4978af782c198f0a136e934916f92cccffb4
Abdullah-FSWD <ansarimdabdullah794@gmail.com>
2023-12-13 11:35:56 +0530

    created store using context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,38 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import { LinkContainer } from "react-router-bootstrap";
+import Nav from "react-bootstrap/Nav";
+import Badge from "react-bootstrap/Badge";
+import { useContext } from "react";
+import { Store } from "./Store";
 
 import "./App.css";
 
 function App() {
+  const { state } = useContext(Store);
+  const { cart } = state;
   return (
     <BrowserRouter>
       <div className="d-flex flex-column site-container">
         <header>
           <Navbar bg="dark" variant="dark">
-          <Container className="mt-3">
+            <Container className="mt-3">
               <LinkContainer to="/">
                 <Navbar.Brand>Tech Eagle</Navbar.Brand>
               </LinkContainer>
+              <Nav className="me-auto">
+                <Link to="/cart" className="nav-link">
+                  Cart
+                  {cart.cartItems.length > 0 && (
+                    <Badge pill bg="danger">
+                      {cart.cartItems.length}
+                    </Badge>
+                  )}
+                </Link>
+              </Nav>
             </Container>
           </Navbar>
         </header>
