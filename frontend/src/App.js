6be03e7955623b5091a69a2148ea4cd50ca6c09e
Abdullah-FSWD <ansarimdabdullah794@gmail.com>
2023-12-13 11:14:33 +0530

    added bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,36 @@
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import { LinkContainer } from "react-router-bootstrap";
 
 import "./App.css";
 
 function App() {
   return (
-       <BrowserRouter>
-      <div>
+    <BrowserRouter>
+      <div className="d-flex flex-column site-container">
         <header>
-          <Link to="/">ShopNow</Link>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>Tech Eagle</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path="/product/:slug" element={<ProductPage />} />
-            <Route path="/" element={<HomePage />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductPage />} />
+              <Route path="/" element={<HomePage />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className="text-center">All right reserved</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
